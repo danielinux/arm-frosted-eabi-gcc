a24c0aed9a9026ffadbbd931dde985a4eb614726
Daniele Lacamera <dan@danielinux.net>
2025-08-16 21:09:54 +0200

    To latest crosstool-ng + workflow build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo $THIS_DIR
 
 mkdir $PWD/cmd
 cd crosstool-ng
-autoreconf -i -f
+./bootstrap
 ./configure --prefix=$PWD/../cmd && make && make install
 cd ..
 
@@ -15,5 +15,10 @@ cp arm-frosted-eabi.config.in .config
 cd newlib/libgloss/arm-frosted
 autoreconf2.69 -i -f
 cd ../../..
+
+cd elf2flt
+git stash
+patch -p1 < ../patches/elf2flt/elf2flt-frosted-patch.diff
+cd ..
 
 cmd/bin/ct-ng build $@
